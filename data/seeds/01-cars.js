df31aae6dadc0a69cf933e3e65bd40d492813c2d
Addow Abdi <adowabdi007@icloud.com>
2025-02-19 09:14:53 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: '1111111111111',
+        vin: '2MRZA21215BJ19873',
         make: 'toyota',
         model: 'prius',
         mileage: 215000,
@@ -9,14 +9,14 @@ const cars = [
         transmission: 'manual',
     },
     {
-        vin: '2222222222222',
+        vin: '3C3CFFAR1CT243601',
         make: 'toyota',
         model: 'corolla',
         mileage: 115000,
         title: 'salvage',
     },
     {
-        vin: '3333333333333',
+        vin: '1FTYR44E03PB14145',
         make: 'ford',
         model: 'focus',
         mileage: 15000,
